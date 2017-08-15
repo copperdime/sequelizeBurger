592215c65021c046fa703c7fb998f13c1673c7e2
copperdime <mrs.kelly@gmail.com>
2017-08-15 12:57:50 -0700

    All the recent updates from previous project

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+
+use burger_db;
+
+CREATE TABLE burgers(
+id INTEGER(10) AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN,
+date TIMESTAMP,
+PRIMARY KEY(id)
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO burgers(burger_name, devoured)
+values("MountainMike", false);
+
+INSERT INTO burgers(burger_name, devoured)
+values("JerseyMike", false);
+
+INSERT INTO burgers(burger_name, devoured)
+values("MikeHardLemonade", false);
+
+INSERT INTO burgers(burger_name, devoured)
+values("MikeBike", false);
+
+INSERT INTO burgers(burger_name, devoured)
+values("MikeAuto", true);
